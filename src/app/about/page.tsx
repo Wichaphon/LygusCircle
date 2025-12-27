14011643fac73dd5116cea7df48106a6c012896a
Wichaphon <horokindov3310@gmail.com>
2025-12-27 17:07:29 +0700

    refactor: update home page and remove unused about page

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AboutPage() {
-  return (
-    <div>Hello form about</div>
-  )
-}
-
-export default AboutPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+
+import { Moon } from 'lucide-react';
+import ThemeMode from '@/components/ThemeMode';
 
 export default function Home() {
   return (
-    <div>
-      Hello World!!
+    <div className='m-4'>
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <Button variant={"secondary"}>Click Me</Button>
+      <ThemeMode />
     </div>
   );
 }
