a1951f66fa17459f174a29900aaf7e3ead5714e9
Wichaphon <horokindov3310@gmail.com>
2026-01-05 22:29:28 +0700

    feat: integrate responsive Sidebar into main layout grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -37,7 +38,7 @@ export default function RootLayout({
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:col-span-3">
-                      Sidebar
+                      <Sidebar />
                     </div>
                     <div className="lg:col-span-9">
                       {children}
